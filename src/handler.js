b08f4c8d8372b360906edeed78f8f8ed3ff4c34d
Inal Mahpud <inalmahpud1@gmail.com>
2023-06-01 13:46:22 +0700

    memperbaiki pada fungsi tambah data buku agar properti name harus ada dan jumlah halaman harus lebih kecil dari jumlah baca

--- src/handler.js
+++ src/handler.js
@@ -8,18 +8,20 @@ const addBooksHandler = (request, h) => {
     const id = nanoid(15);
     const insertedAtd = new Date().toISOString();
     const updatedAt = insertedAtd;
-    let finished = false
+    let finished = false;
     if(pageCount === readPage) { finished = true}
-
     const newBooks = {
         id, name, year, author, summary, publisher, pageCount, readPage,finished, reading, updatedAt
     }
 
     books.push(newBooks);
 
-    const isSuccess = books.filter((book) => book.id === id).length > 0;
-
+    const isSuccess = books.filter((book) => book.id === id).length > 0
+    // const haveName =  books.filter((book) => book.name === name).length > 0;
+    
     if(isSuccess) {
+        if (name) {
+            if (pageCount >= readPage) {
         const response = h.response({
             status: 'success',
             message: 'Buku berhasil ditambahkan',
@@ -30,6 +32,20 @@ const addBooksHandler = (request, h) => {
         response.code(201);
         return response
     }
+    const response = h.response({
+        status: 'fail',
+        message: 'Gagal menambahkan buku. readPage tidak boleh lebih besar dari pageCount',
+    })
+    response.code(400);
+    return response
+}
+    const response = h.response({
+        status: 'fail',
+        message: 'Gagal menambahkan buku. Mohon isi nama buku',
+    })
+    response.code(400);
+    return response
+}
     const response = h.response({
         status: 'fail',
         message: 'Data gagal ditambahkan',
